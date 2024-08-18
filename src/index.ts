0efb2ef28465304ece940eaf7b984105be20b550
Francisco Martinez <franciscomj345@gmail.com>
2024-08-18 15:30:02 -0600

    Add timeout

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,15 @@ export async function handler(
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> {
   console.log("in handler");
+
+  const message = await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve("Time out complete");
+    }, 2000),
+  );
+
   return {
     statusCode: 200,
-    body: "Hello world!",
+    body: JSON.stringify(message),
   };
 }
